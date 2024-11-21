626f8ee5457c924e86ad9b8d6d0aa1c964d73afb
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2024-11-21 19:53:21 +0530

    food dilivery

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='app'>
+        <Navbar/>
+       </div>
+    </>
+  )
+}
+
+export default App
